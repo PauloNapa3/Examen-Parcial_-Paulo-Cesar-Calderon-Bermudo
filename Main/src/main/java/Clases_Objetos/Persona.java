5d02897d9b43af8d4752c5c4979dc3b086c4d9c5
Paulo <paulocesar.calderon@unmsm.edu.pe>
2022-07-08 21:14:19 -0500

    Se crea la clase persona , se definen sus atributos y metodos

---
+++ Main/src/main/java/Clases_Objetos/Persona.java
@@ -0,0 +1,19 @@
+package Clases_Objetos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Calderón Bermudo
+ */
+public class Persona {
+//Atributos
+    int DNI;
+    String Nombre;
+//Metodo
+    public static void main(String[] args) {
+Persona persona1 = new Persona();
+persona1.DNI= 
+var consola = new Scanner(System.in);
+    }
+}
