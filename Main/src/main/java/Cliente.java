69981b013e9212a12880f91df912df5b26c14c96
Paulo <paulocesar.calderon@unmsm.edu.pe>
2022-07-08 20:24:49 -0500

    Se crea la clase cliente

--- Main/src/main/java/Cliente.java
+++ Main/src/main/java/Cliente.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author Usuario
+ * @author Paulo Cesar Calderón Bermudo
  */
 public class Cliente {
     public static void main (String args[]){
